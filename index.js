a30f88d6564b4b7cdb438f270db2ea1b008f650d
kez <zabaluevmark92@gmail.com>
2025-04-18 00:05:07 +0300

    переехал на мак - проверка

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
-
-let iPhone = 2007;
-alert('год выпуска первого iPhone ' + iPhone);
-
-let  creatorJS= 'Брендан Эйх'
-alert('создатель языка JavaScript  ' + creatorJS);
-
-const x = 10;
-const y = 2;
-alert('сумма чисел а и b = ' + (x+y) + "\n" + 'разность = ' + (x-y) + "\n" + 'произведение = '+ (x * y) + "\n" + 'частное = ' + (x/y));
-
-let resalt = 2 ** 5
-alert('2 в 5й степени = ' + resalt);
-
-
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+//
+// let iPhone = 2007;
+// alert('год выпуска первого iPhone ' + iPhone);
+//
+// let  creatorJS= 'Брендан Эйх'
+// alert('создатель языка JavaScript  ' + creatorJS);
+//
+// const x = 10;
+// const y = 2;
+// alert('сумма чисел а и b = ' + (x+y) + "\n" + 'разность = ' + (x-y) + "\n" + 'произведение = '+ (x * y) + "\n" + 'частное = ' + (x/y));
+//
+// let resalt = 2 ** 5
+// alert('2 в 5й степени = ' + resalt);
+//
+//
 let z = 9;
 let p = 2;
 alert('остаток от деления 9 и 2 = ' + z % p);
