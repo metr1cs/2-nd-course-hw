b371cf9aaf6f43e8b3a822b77123de53246975ce
kez <zabaluevmark92@gmail.com>
2025-04-20 16:02:14 +0300

    наал делать домашку 7

---
+++ hw-7.js
@@ -0,0 +1,7 @@
+function toUpperCaseString(inputString) {
+    return inputString.toUpperCase();
+}
+
+// Пример использования:
+const userInput = prompt("Введите текст:");
+console.log(toUpperCaseString(userInput)); // Выводит введённый текст в верхнем регистре
