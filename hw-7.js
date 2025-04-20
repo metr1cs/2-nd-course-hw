b631df97005c146ccd2358a1a9e2b2209a8d0640
kez <zabaluevmark92@gmail.com>
2025-04-20 16:29:22 +0300

    сделал задача 2/3/4

--- hw-7.js
+++ hw-7.js
@@ -1,5 +1,28 @@
+// Задача 1
 function toUpperCaseString(inputString) {
     return inputString.toUpperCase();
 }
 const userInput = prompt("Введите текст:");
 console.log(toUpperCaseString(userInput));
+
+// Задача 2
+function filterStringsByPrefix(array, prefix) {
+    const lowerPrefix = prefix.toLowerCase();
+
+    return array.filter(str =>
+        str.toLowerCase().startsWith(lowerPrefix)
+    );
+}
+
+// Задача 3
+const number = 32.58884;
+
+function numbers(number) {
+    console.log(Math.floor(number));
+    console.log(Math.ceil(number));
+    console.log(Math.round(number));
+}
+
+numbers(number);
+
+// Задача 4
