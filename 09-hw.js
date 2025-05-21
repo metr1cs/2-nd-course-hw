4a7df4b8290ee39884b5e5f38e76e45562f2fef2
kez <zabaluevmark92@gmail.com>
2025-05-22 02:13:52 +0300

    решил первую задачу

---
+++ 09-hw.js
@@ -0,0 +1,20 @@
+
+
+const descriptionEl = document.querySelector('.description');
+const btnEl = document.querySelector('#btn');
+let originalDiscription = descriptionEl.textContent;
+
+// btnEl.addEventListener('click',function (e){
+//     console.log(1);
+// })
+
+btnEl.addEventListener('click',function (){
+    if (descriptionEl.textContent === ""){
+        descriptionEl.textContent = originalDiscription;
+    }
+    else{
+        descriptionEl.textContent = '';
+    }
+
+
+})
